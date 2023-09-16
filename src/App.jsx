6fe7b00557fd6b69a086733326f7662935636dea
Garakoti <rahulsinghgarakoti730@gmail.com>
2023-09-16 20:59:57 +0530

    adding the nav bar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+  return <div className="container">
+  <Navigation/>
+  </div>;
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import styles from "./navbar.module.css";
+const Navigation = () => {
+
+  return (
+    <div className="container">
+      <div className={`${styles.navbar}` }>
+        <img src="./Images/logo.png" alt="" />
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
